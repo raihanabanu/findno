0e5e6902236941f8ec638ca5ee1a6979767d21be
raihanabanu <raihanabanu.s@gmail.com>
2016-07-19 10:03:26 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,15 @@
+# findno
+class Odd
+{
+public static void main(String args[])
+{
+int i=integer.parseInt(args[0]);
+if(i%2==0)
+{
+System.out.println("the givennumber is even");
+}
+else{
+System.out.println("the given number is odd");
+}
+}
+}
